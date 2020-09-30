59496df1c678b2193f4648407e1fb4d3db67b82a
yhu71 <yhu71@stevens.edu>
2020-09-29 21:19:33 -0400

    Initial commit

---
+++ CheckRepoAndCommits.py
@@ -0,0 +1,31 @@
+"""
+Author: Ying Hu
+This is for Homework 04a - Develop with the Perspective of the Tester in mind
+"""
+
+import requests
+import json
+
+
+def check_repo_and_commits(github_user_id):
+    # Use a dictionary to store the result, key = repo name, value = number of commits
+    repository = {}
+    # Retrieving a user's repositories:
+    user_repos = requests.get(f'https://api.github.com/users/{github_user_id}/repos')
+    parsed_user_repos = json.loads(user_repos.text)
+
+    # Retrieving the commits of a repository:
+    for repo in parsed_user_repos:
+        repo_name = repo["name"]
+
+        repo_commits = requests.get(f'https://api.github.com/repos/{github_user_id}/{repo_name}/commits')
+        repo_commit = json.loads(repo_commits.text)
+
+        repository[repo_name] = len(repo_commit)
+        print(f'Repo: {repo_name} Number of commits: {len(repo_commit)}')
+
+    return repository
+
+
+if __name__ == '__main__':
+    check_repo_and_commits('yhu71')
---
+++ CheckRepoAndCommits_test.py
@@ -0,0 +1,16 @@
+"""
+Author: Ying Hu
+This is for Homework 04a - Develop with the Perspective of the Tester in mind
+"""
+import unittest
+from CheckRepoAndCommits import check_repo_and_commits
+
+
+class CheckRepoAndCommitsTest(unittest.TestCase):
+    def test_check_repo_and_commits(self):
+        expected = {'ssw555tmHogwarts2020Spring': 3, 'Triangle567': 9}
+        self.assertEqual(check_repo_and_commits('yhu71'), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
