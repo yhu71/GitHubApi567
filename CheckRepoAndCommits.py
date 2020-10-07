98654036de580b991bffe04b7586ff5a98cd2749
yhu71 <yhu71@stevens.edu>
2020-10-07 15:47:59 -0400

    reverse

--- CheckRepoAndCommits.py
+++ CheckRepoAndCommits.py
@@ -33,12 +33,21 @@ def check_repo_and_commits(github_user_id):
     # Use a dictionary to store the result, key = repo name, value = number of commits
     repository = {}
     # Retrieving a user's repositories:
+<<<<<<< HEAD
     user_repos = get_repos(github_user_id)
+=======
+    user_repos = requests.get(f'https://api.github.com/users/{github_user_id}/repos')
+>>>>>>> parent of b31e066... update test
     parsed_user_repos = json.loads(user_repos.text)
     # Retrieving the commits of a repository:
     for repo in parsed_user_repos:
         repo_name = repo["name"]
+<<<<<<< HEAD
         repo_commits = get_commits(github_user_id, repo_name)
+=======
+
+        repo_commits = requests.get(f'https://api.github.com/repos/{github_user_id}/{repo_name}/commits')
+>>>>>>> parent of b31e066... update test
         repo_commit = json.loads(repo_commits.text)
         repository[repo_name] = len(repo_commit)
         print(f'Repo: {repo_name} Number of commits: {len(repo_commit)}')
